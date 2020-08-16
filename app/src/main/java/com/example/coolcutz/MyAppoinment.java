3ca2c1ab1f2261d092eb5c3535a225378d3dcc91
Dhara942 <dharanethmi@gmail.com>
2020-08-17 02:35:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/coolcutz/MyAppoinment.java
@@ -0,0 +1,14 @@
+package com.example.coolcutz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyAppoinment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_appoinment);
+    }
+}
